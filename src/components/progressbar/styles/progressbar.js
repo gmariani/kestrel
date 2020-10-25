73cbf9c5de008d8ccc0cb3207e48b50c5aafe094
Gabriel <gabriel@coursevector.com>
2020-10-25 00:58:41 -0400

    styled progress bar

--- src/components/progressbar/styles/progressbar.js
+++ src/components/progressbar/styles/progressbar.js
@@ -3,11 +3,11 @@ import styled from 'styled-components/macro';
 export const Track = styled.div`
     height: 5px;
     width: 100%;
-    background-color: #c4c4c4;
+    background-color: rgba(255, 255, 255, 0.25);
 `;
 
 export const Bar = styled.div`
     height: 5px;
     width: ${({ value }) => `${value}%;`};
-    background: linear-gradient(90deg, #3b79c7 0%, #3d5b81 100%);
+    background: white;
 `;
